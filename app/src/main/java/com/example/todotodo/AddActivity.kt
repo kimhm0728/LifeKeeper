d2e28154a7827fb8aa1700c6b2a71a67fb0df184
kimhm0728 <kimhm0728@naver.com>
2023-09-17 21:04:30 +0900

    feat: 할일 추가 기능 구현

--- app/src/main/java/com/example/todotodo/AddActivity.kt
+++ app/src/main/java/com/example/todotodo/AddActivity.kt
@@ -1,7 +1,9 @@
 package com.example.todotodo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.MotionEvent
+import android.view.Window
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.todotodo.database.Todo
@@ -10,8 +12,10 @@ import com.example.todotodo.databinding.ActivityAddBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.util.*
 
 class AddActivity : AppCompatActivity() {
 
@@ -20,6 +24,8 @@ class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+
         binding = ActivityAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -27,22 +33,24 @@ class AddActivity : AppCompatActivity() {
     }
 
     private fun composeUI() {
-        val today = LocalDate.now().toString()
+        val today = SimpleDateFormat("yyyy / MM / dd").format(Date())
         binding.dateText.text = today
 
         binding.calendar.setOnDateChangeListener { _, year, month, day ->
-            val date = "$year-${month + 1}-$day"
-            binding.dateText.text = date
+            val date = getDateStr(year, month + 1, day)
 
             if (date < today) {
                 showToast("이전 날짜는 선택하실 수 없습니다!")
+                return@setOnDateChangeListener
             }
+
+            binding.dateText.text = date
         }
 
         binding.addBtn.setOnClickListener {
             val contents = binding.editText.text.toString()
             if (contents == "") {
-                showToast("할일 내용을 입력해주세요.")
+                showToast("할일의 내용을 입력해주세요.")
                 return@setOnClickListener
             }
 
@@ -53,6 +61,22 @@ class AddActivity : AppCompatActivity() {
                 db?.todoDao()?.insert(newTodo)
             }
         }
+    }
+
+    private fun getDateStr(year: Int, month: Int, day: Int): String {
+        var date = "$year / "
+
+        if (month < 10) date += "0"
+        date += "$month / "
+
+        if (day < 10) date += "0"
+        date += day
+
+        return date
+    }
+
+    override fun onBackPressed() {
+        //super.onBackPressed
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
