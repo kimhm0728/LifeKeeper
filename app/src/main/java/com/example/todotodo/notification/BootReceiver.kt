c902334e96aa64b217cf9f4651d95e519d1cdd2c
kimhm0728 <kimhm0728@naver.com>
2023-09-25 23:02:36 +0900

    feat: boot notification 수정

--- app/src/main/java/com/example/todotodo/notification/BootReceiver.kt
+++ app/src/main/java/com/example/todotodo/notification/BootReceiver.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import com.example.todotodo.NotificationActivity.Companion.DEFAULT_KEY
 import com.example.todotodo.NotificationActivity.Companion.HOUR_KEY
 import com.example.todotodo.NotificationActivity.Companion.MINUTE_KEY
+import com.example.todotodo.NotificationActivity.Companion.SWITCH_KEY
 import com.example.todotodo.preference.PreferenceUtil
 import com.example.todotodo.preference.TodoApplication
 
@@ -15,17 +16,19 @@ class BootReceiver : BroadcastReceiver() {
     private lateinit var pref: PreferenceUtil
 
     override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "BOOT_COMPLETED onReceive()")
+
         pref = TodoApplication.prefs
-
-        Log.d("BOOT", "start")
-        if (intent.action != "android.intent.action.BOOT_COMPLETED") return
-
-        Log.d("BOOT", "endddd")
+        if (!pref.getData(SWITCH_KEY, DEFAULT_KEY).toBoolean()) return
 
         val h = pref.getData(HOUR_KEY, DEFAULT_KEY)
         val m = pref.getData(MINUTE_KEY, DEFAULT_KEY)
 
         if (h == DEFAULT_KEY || m == DEFAULT_KEY) return
         NotificationManager(context).startNotification(h.toInt(), m.toInt())
+    }
+
+    companion object {
+        private val TAG = BootReceiver::class.simpleName
     }
 }
